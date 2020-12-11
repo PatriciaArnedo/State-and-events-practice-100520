1c189badbafd15e598356e2ca263bce03d6f5517
PatriciaArnedo <paaarnedo@gmail.com>
2020-12-10 19:17:56 -0500

    commit tree 3d9165c8198cd6e1ce62b1a589e17471f6092b17

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -1,17 +1,46 @@
 import React from "react";
 
-function DogCard() {
-  return (
-    <div className="card">
-      <span className="content">
-        <h2 >{/*Dog name goes here*/}</h2>
-        <img alt="" src={""} />
-      </span>
-      <span className="bark">
-        <button>Bark</button>
-      </span>
-    </div>
-  );
+class DogCard extends React.Component {
+  
+  clickHandler = () => {
+    if(!this.state.clicked){
+      this.setState(
+        {
+          clicked: true,
+          h2Text: "Bark"
+        }
+      )
+    } else {
+      this.setState(
+        {
+          clicked: false,
+          h2Text: ""
+        }
+      )
+    }
+  }
+
+  state ={
+    clicked: false,
+    h2Text: ""
+  }
+  
+  render(){
+    return (
+      <div className="card">
+        <span className="content">
+          <h2 >{this.props.dogObj.name}</h2>
+          <img alt="dog" src={this.props.dogObj.img} />
+        </span>
+        <span className="bark">
+          <button onClick={this.clickHandler} >Bark</button>
+        </span>
+        <span>
+          <h2 className="bark">{this.state.h2Text}</h2>
+        </span>
+      </div>
+    )
+  }
 }
 
 export default DogCard;
